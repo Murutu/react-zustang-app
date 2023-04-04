fd6b5ed3c3dcd244118905c38fbc747b5bfa8a91
Murutu <murutupeter@gmail.com>
2023-04-04 12:57:06 +0300

    style: styled register, reset & password page

--- react-ojar/src/helper/validate.js
+++ react-ojar/src/helper/validate.js
@@ -11,6 +11,48 @@ export async function usernameValidate(values) {
 }
 
 /*
+Validate password 
+*/
+export async function passwordValidate(values) {
+    const errors = passwordVerify({}, values)
+
+    return errors;
+}
+
+/*
+Validate reset password
+*/
+
+export async function resetPasswordValidation(values) {
+    const errors = passwordVerify({}, values);
+
+    if(values.password !== values.confirm_pwd) {
+        errors.exist = toast.error("Password not match...!")
+    }
+
+    return errors;
+}
+
+/*
+Verify password 
+*/
+
+function passwordVerify(errors ={}, values) {
+
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+    if(!values.password) {
+        errors.password = toast.error("Password Required...!")
+    } else if(values.password.includes(" ")) {
+        errors.password = toast.error("Wrong Password...!")
+    } else if(values.password.length < 4) {
+        errors.password = toast.error("Password must be more than 6 characters!")
+    } else if(!specialChars.test(values.password)) {
+        errors.password = toast.error("Password must have special character")
+    }   
+}
+
+/*
 Validate Username 
 */
 
